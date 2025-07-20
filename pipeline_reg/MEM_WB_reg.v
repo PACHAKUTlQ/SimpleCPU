f2930fe0f97e4223b105f2ff8cc78c1402d80153
Pachakutiq <101460915+PACHAKUTlQ@users.noreply.github.com>
2025-07-20 11:46:44 +0800

    fix(MEM/WB reg)!: Update datapaths when supporting WB writeback of jumping

--- pipeline_reg/MEM_WB_reg.v
+++ pipeline_reg/MEM_WB_reg.v
@@ -5,15 +5,19 @@ module MEM_WB_reg (
     input rst,
 
     // Inputs from MEM Stage
+    input [31:0] mem_pc,
     input mem_memToReg,
     input mem_regWrite,
+    input [1:0] mem_jumpType,  // 01: jalr, 10: jal
     input [31:0] mem_dataFromRAM,  // readData from RAM
     input [31:0] mem_ALUResult,
     input [4:0] mem_rd,  // Instruction [11:7]
 
     // Outputs to WB Stage
+    output reg [31:0] wb_pc,
     output reg wb_memToReg,
     output reg wb_regWrite,
+    output reg [1:0] wb_jumpType,
     output reg [31:0] wb_dataFromRAM,
     output reg [31:0] wb_ALUResult,
     output reg [4:0] wb_rd
@@ -21,14 +25,18 @@ module MEM_WB_reg (
 
   always @(posedge clk or posedge rst) begin
     if (rst) begin
+      wb_pc <= 32'b0;
       wb_memToReg <= 1'b0;
       wb_regWrite <= 1'b0;
+      wb_jumpType <= 2'b00;
       wb_dataFromRAM <= 32'b0;
       wb_ALUResult <= 32'b0;
       wb_rd <= 5'b0;
     end else begin
+      wb_pc <= mem_pc;
       wb_memToReg <= mem_memToReg;
       wb_regWrite <= mem_regWrite;
+      wb_jumpType <= mem_jumpType;
       wb_dataFromRAM <= mem_dataFromRAM;
       wb_ALUResult <= mem_ALUResult;
       wb_rd <= mem_rd;
