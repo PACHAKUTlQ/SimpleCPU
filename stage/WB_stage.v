fcf850e0bae1906cf50f20ad2e042d94140a308d
Pachakutiq <101460915+PACHAKUTlQ@users.noreply.github.com>
2025-07-20 11:45:40 +0800

    fix(WB)!: Support writeback of jal and jalr

--- stage/WB_stage.v
+++ stage/WB_stage.v
@@ -1,15 +1,30 @@
 `timescale 1ns / 1ps
 
 module WB_stage (
-    input [31:0] PCPlus4,
+    input [31:0] wb_pc,
     input [31:0] dataFromRAM,
     input [31:0] ALUResult,
     input memToReg,
     input [1:0] jumpType,  // 01: jalr, 10: jal
 
-    output [31:0] writeData
+    output reg [31:0] writeData
 );
 
-  assign writeData = (jumpType != 2'b00) ? dataFromRAM : PCPlus4;  // If jal or jalr, write PC+4
+  always @(*) begin
+    if (memToReg) begin
+      $display("memToReg is 1, jumpType: %b", jumpType);
+      if (jumpType == 2'b00) begin  // lw, lb, lbu
+        $display("lw/lb/lbu: dataFromRAM: %h", dataFromRAM);
+        writeData = dataFromRAM;
+      end else if (jumpType == 2'b01 || jumpType == 2'b10) begin  // jal, jalr
+        $display("jal/jalr: wb_pc + 4: %h", wb_pc + 4);
+        writeData = wb_pc + 4;
+      end else begin
+        writeData = 32'b0;
+      end
+    end else begin
+      writeData = ALUResult;
+    end
+  end
 
 endmodule
